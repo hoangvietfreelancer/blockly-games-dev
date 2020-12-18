8d047236554bfc12bcd336d7243af734f36c2c16
hoangvietfreelancer <29736874+hoangvietfreelancer@users.noreply.github.com>
2020-12-18 17:22:19 +0700

    Update languages and third-party

--- pond/docs/generated/vi/uncompressed.js
+++ pond/docs/generated/vi/uncompressed.js
@@ -1,5 +1,5 @@
 // Automatically generated file.  Do not edit!
- // This version is modified by viet1pro, Komaz from MIS!
+// This version is modified by viet1pro, Komaz, Baonamdao from MIS!
 
 
 window.CLOSURE_NO_DEPS = true;
